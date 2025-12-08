1f638657f8a6ff07bd7f1e45ec678a732c48ae23
kennedy-ak <akogokennedy@gmail.com>
2025-12-08 20:28:34 +0000

    allowed host

--- myhub/settings.py
+++ myhub/settings.py
@@ -28,6 +28,8 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,system.digitalrepublic.space', cast=Csv())
 
+CSRF_TRUSTED_ORIGINS = ['https://system.digitalrepublic.space']
+
 
 # Application definition
 
